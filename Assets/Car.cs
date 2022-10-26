57a9a5a65d89517d9786ea0b18ab68b9c46aa339
sinartz <yasin.cobanoglu77@gmail.com>
2022-10-26 22:29:04 +0300

    Initial commit

---
+++ Assets/Car.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Car : MonoBehaviour
+{
+    [SerializeField] float speed;
+    public bool forward;
+    [SerializeField] Transform carParent;
+    [SerializeField] GameManager _gameManager;
+    [SerializeField] bool stopPointd = false;
+     
+
+    
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (!stopPointd)
+            transform.Translate(new Vector3(-2f * Time.deltaTime, 0, 0));
+        if (forward)
+            transform.Translate(new Vector3(-speed * Time.deltaTime,0,0)); 
+        
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Parking"))
+        {
+            forward = false;
+            transform.SetParent(carParent);
+            _gameManager.NewCarSummon();            
+        }
+        if (collision.gameObject.CompareTag("Gobek"))
+        {
+            
+            Destroy(gameObject);
+            _gameManager.NewCarSummon();
+
+        }
+        else if (collision.gameObject.CompareTag("StopPoint"))
+        {
+            stopPointd = true;
+        }
+        
+
+            
+    }
+}
---
+++ Assets/GameManager.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [Header("Car Settings")]
+    [SerializeField] GameObject[] car;
+    [SerializeField] int ActiveCarIndex=0;
+    [SerializeField] int HowMuchCar;
+
+
+    [Header("Platform Settings")]
+    [SerializeField] GameObject platform_1;
+    [SerializeField] GameObject platform_2;
+    [SerializeField] float[] donusHizlari;
+
+    void Start()
+    {
+        car[ActiveCarIndex].SetActive (true);
+        
+    }
+
+    public void NewCarSummon()
+    {
+        if (ActiveCarIndex < HowMuchCar)
+        {
+            car[ActiveCarIndex].SetActive(true);
+        }
+        else
+        {
+            Debug.Log("Araç bitti");
+        }
+            
+    }
+    void Update()
+
+    {        
+        if (Input.GetKeyDown(KeyCode.W)) 
+        {
+        car[ActiveCarIndex].GetComponent<Car>().forward = true;
+        ActiveCarIndex++;
+        }        
+        platform_1.transform.Rotate(new Vector3(0,0,donusHizlari[0]), Space.Self);
+    }
+    
+    
+}
